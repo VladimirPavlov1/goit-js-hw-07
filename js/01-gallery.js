3289c1f6f6bb23cac0b65fe6eb618bd59bb6e353
VladimirPavlov1 <pavvvlovvvvvvladimir@gmail.com>
2022-10-26 10:23:26 +0300

    виконав 1 завдання

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,7 +1,7 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 // import * as basicLightbox from 'basiclightbox'
-console.log(galleryItems);
+
 const bodyEl= document.querySelector('body')
 const galleryList =document.querySelector('.gallery')
 const markupEl=markupElemGallery(galleryItems)
@@ -26,23 +26,20 @@ function markupElemGallery(galleryItems){
 }).join('');
 }
 galleryList.insertAdjacentHTML('beforeend',markupEl);
-console.log(galleryList)
 const linkEl=document.querySelector('.gallery__link');
-console.dir(linkEl)
 const imgEl=document.querySelectorAll('.gallery__image')
 galleryList.addEventListener('click',onDivGalleryClick)
 
+let instance=""
 function onDivGalleryClick(event){
     event.preventDefault()
-    
+
    if(event.target.nodeName!=='IMG'){
     return
    }
-   console.dir(event.target.nodeName)
-   console.dir(event.target)
-    console.log(event.currentTarget)
+ 
    const largeImg=event.target.dataset.source
-   return basicLightbox.create(
+     instance =basicLightbox.create(
     `
     <div class="modal">
         
@@ -54,50 +51,18 @@ function onDivGalleryClick(event){
      
      </div>
     `
-   ).show()
+   )
+   instance.show()
     
+document.addEventListener('keydown',onBackDropClosePressEsc);
+   function onBackDropClosePressEsc(event){
+    
+    if (event.code === "Escape") {
+       instance.close()
+        document.removeEventListener('keydown', onBackDropClosePressEsc);
+   }
+}
 }
 
 
 
-
-
-// import * as basicLightbox from 'basiclightbox'
-
-// const instance = basicLightbox.create(`
-//     <div class="modal">
-//         <p>
-//             Your first lightbox with just a few lines of code.
-//             Yes, it's really that simple.
-//         </p>
-//     </div>
-// `)
-
-// instance.show()
-
-
-
-
-
-
-
-
-
-// const instance = basicLightbox.create(`
-// <div class="gallery__item">
-// <a class="gallery__link" href="large-image.jpg">
-//   <img
-//     class="gallery__image"
-//     src="small-image.jpg"
-//     data-source="large-image.jpg"
-//     alt="Image description"
-//   />
-// </a>
-// </div>
-// `)
-
-
-
-// instance.show()
-// const basicLightbox = require('basiclightbox')
-// const visible = basicLightbox.visible()
